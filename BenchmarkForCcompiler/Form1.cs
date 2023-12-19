3d25b6e0e9a136d32c86214db0a217ef27cabfac
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-12-19 09:43:16 +0900

    外部アプリケーション呼び出し完了

--- BenchmarkForCcompiler/Form1.cs
+++ BenchmarkForCcompiler/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace BenchmarkForCcompiler
 {
@@ -20,6 +21,24 @@ namespace BenchmarkForCcompiler
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ProcessStartInfoのオブジェクトを生成
+            ProcessStartInfo psInfo = new ProcessStartInfo();
+            psInfo.FileName = @"C:\\Users\\sotar\\Desktop\\BenchmarkForCcompiler\\testcase\\a.exe"; //コマンド
+            // psInfo.Arguments = @"/c dir d:\ /s"; //引数
+            psInfo.CreateNoWindow = true; // コンソール・ウィンドウを開かない
+            psInfo.UseShellExecute = false; // シェル機能を使用しない
+            psInfo.RedirectStandardOutput = true; // 標準出力をリダイレクト
+
+            Process p = Process.Start(psInfo); // コマンドの実行開始
+            string line = "";
+            while((line = p.StandardOutput.ReadLine()) != null)
+            {
+                textBox1.Text = line;
+            }
         }
     }
 }
